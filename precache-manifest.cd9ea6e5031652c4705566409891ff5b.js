631a375cac0ed25bdc130b99ade2ac704677fa13
changangus <63685366+changangus@users.noreply.github.com>
2020-10-15 11:45:49 -0400

    Updates

---
+++ precache-manifest.cd9ea6e5031652c4705566409891ff5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e7b3c66c5e7fbb3ff5f45b5f5de93cd",
+    "url": "/React_Joke_Teller/index.html"
+  },
+  {
+    "revision": "7253179640ad03d1211c",
+    "url": "/React_Joke_Teller/static/css/main.1d4af40d.chunk.css"
+  },
+  {
+    "revision": "105bca8dcb3298922818",
+    "url": "/React_Joke_Teller/static/js/2.d417a2c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_Joke_Teller/static/js/2.d417a2c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7253179640ad03d1211c",
+    "url": "/React_Joke_Teller/static/js/main.1a518bef.chunk.js"
+  },
+  {
+    "revision": "15098667b02ea2219de1",
+    "url": "/React_Joke_Teller/static/js/runtime-main.5c2a0e76.js"
+  }
+]);
